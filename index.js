447992b045586591325bed8902e5504c43303ec9
Jerome T.K. Covington <jeromecovington@gmail.com>
2018-08-23 09:23:09 -0400

    Update comments, naming, & arguments

--- index.js
+++ index.js
@@ -1,50 +1,73 @@
 // The possible methods.
-const METHODS = [
+const METHOD_OPERATORS = [
   '+',
   '-',
   '*',
   '/'
 ];
 
-// State representing guesses that have been made.
-let guesses;
-
-// State representing method that has been leart.
+// State Object with shape `{operator: <string>, suffix: <number>}`
+// representing method that has been learnt.
 let learntMethod;
 
 /**
- * Prompts first for a number representing input, then for a number representing output.
+ * Prompts first for a number representing input, then for a number representing
+ * output.
  *
  * @returns {Object} Object with shape `{in: <number>, out: <number>}`
  */
-async function prompt() {
+async function promptGuess() {
 
 }
 
 /**
- * Attempts to derive the mathematical method used to derive one number from another.
- * If unsuccessful, or if not enout information available, prompts for another pair.
+ * Attempts to derive the mathematical method used to derive one number from
+ * another. If unsuccessful, or if not enout information available, prompts for
+ * another pair.
  *
- * @returns {string} String confirming learnt mathematical method
+ * @param {Number} object.in - Input for the training
+ * @param {Number} object.out - Output for the training
+ *
+ * @returns {Object} Object with shape `{operator: <string>, suffix: <number>}`
+ * confirming learnt mathematical method
  */
-function guess({in, out}) {
+export function guess({in, out}) {
+  // Represents guesses that have been made.
+  let guesses;
 
 }
 
 /**
  * Affirms learnt method.
  *
- * @returns {string} String rpresenting learnt method.
+ * @param {string} object.operator - String representing operation
+ * @param {Number} object.suffix - The suffix for the method
+ *
+ * @returns {Object} Object with shape `{operator: <string>, suffix: <number>}`
+ * confirming learnt mathematical method
  */
-function learn(method) {
+function learn({operator, suffix}) {
+
+}
+
+/**
+ * Propmpts for a number representing input.
+ *
+ * @returns {Number} Number representing input
+ */
+async function promptOperate() {
 
 }
 
 /**
  * Operate on input using the learnt mathematical method.
  *
+ * @param {Number} object.in - Input for the operation
+ * @param {string} object.operator - String representing operation
+ * @param {Number} object.suffix - The suffix for the method
+ *
  * @returns {Number} Output of operation
  */
-async function operate(in) {
+export function operate({in, operator, suffix}) {
 
 }
