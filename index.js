c5926945f47656a6cb7310043ae7b44c0bd28afe
Jerome T.K. Covington <jeromecovington@gmail.com>
2018-08-24 08:44:17 -0400

    Conform to standard

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // The possible methods.
-const METHOD_OPERATORS = [
-  '+',
-  '-',
-  '*',
-  '/'
-];
+const METHOD_OPERATORS = {
+  add: '+',
+  subtract: '-',
+  multiply: '*',
+  divide: '/'
+}
 
 // State Object with shape `{operator: <string>, suffix: <number>}`
 // representing method that has been learnt.
-let learntMethod;
+let learntMethod
 
 /**
  * Prompts first for a number representing input, then for a number representing
  * output.
  *
- * @returns {Object} Object with shape `{in: <number>, out: <number>}`
+ * @returns {Object} Object with shape `{input: <number>, output: <number>}`.
  */
-async function promptGuess() {
+async function promptGuess () {
 
 }
 
@@ -25,49 +25,51 @@ async function promptGuess() {
  * another. If unsuccessful, or if not enout information available, prompts for
  * another pair.
  *
- * @param {Number} object.in - Input for the training
- * @param {Number} object.out - Output for the training
+ * @param {Object} object - Parameter object.
+ * @param {number} object.input - Input for the training.
+ * @param {number} object.output - Output for the training.
  *
  * @returns {Object} Object with shape `{operator: <string>, suffix: <number>}`
- * confirming learnt mathematical method
+ * confirming learnt mathematical method.
  */
-export function guess({in, out}) {
+export function guess ({ input, output }) {
   // Represents guesses that have been made.
-  let guesses;
-
+  let guesses
 }
 
 /**
  * Affirms learnt method.
  *
- * @param {string} object.operator - String representing operation
- * @param {Number} object.suffix - The suffix for the method
+ * @param {Object} object - Parameter object.
+ * @param {string} object.operator - String representing operation.
+ * @param {number} object.suffix - The suffix for the method.
  *
  * @returns {Object} Object with shape `{operator: <string>, suffix: <number>}`
- * confirming learnt mathematical method
+ * confirming learnt mathematical method.
  */
-function learn({operator, suffix}) {
+function learn ({ operator, suffix }) {
 
 }
 
 /**
- * Propmpts for a number representing input.
+ * Prompts for a number representing input.
  *
- * @returns {Number} Number representing input
+ * @returns {number} Number representing input.
  */
-async function promptOperate() {
+async function promptOperate () {
 
 }
 
 /**
  * Operate on input using the learnt mathematical method.
  *
- * @param {Number} object.in - Input for the operation
- * @param {string} object.operator - String representing operation
- * @param {Number} object.suffix - The suffix for the method
+ * @param {Object} object - Parameter object.
+ * @param {number} object.input - Input for the operation.
+ * @param {string} object.operator - String representing operation.
+ * @param {number} object.suffix - The suffix for the method.
  *
- * @returns {Number} Output of operation
+ * @returns {number} Output of operation.
  */
-export function operate({in, operator, suffix}) {
+export function operate ({input, operator, suffix}) {
 
 }
