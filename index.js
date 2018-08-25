0d782ff7341bd701eac3190de9f2ab5096ba66dc
Jerome T.K. Covington <jeromecovington@gmail.com>
2018-08-25 07:18:54 -0400

    Function bodies for guessing, learning & operating

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+const promptly = require('promptly')
+
 // The possible methods.
-const METHOD_OPERATORS = {
+const OPERATORS = {
   add: '+',
   subtract: '-',
   multiply: '*',
   divide: '/'
 }
 
+// State Array with members of shape `{operator: <string>, suffix: <number>}`
+// representing methods guessed.
+const guesses = []
+
 // State Object with shape `{operator: <string>, suffix: <number>}`
 // representing method that has been learnt.
-let learntMethod
+const learntMethod = {}
 
 /**
  * Prompts first for a number representing input, then for a number representing
@@ -17,24 +23,64 @@ let learntMethod
  * @returns {Object} Object with shape `{input: <number>, output: <number>}`.
  */
 async function promptGuess () {
+  const input = await promptly.prompt('What is the input?')
+  const output = await promptly.prompt('What is the output?')
 
+  return {
+    input: parseInt(input, 10),
+    output: parseInt(output, 10)
+  }
 }
 
 /**
  * Attempts to derive the mathematical method used to derive one number from
- * another. If unsuccessful, or if not enout information available, prompts for
+ * another. If unsuccessful, or if not enough information available, prompts for
  * another pair.
  *
  * @param {Object} object - Parameter object.
  * @param {number} object.input - Input for the training.
  * @param {number} object.output - Output for the training.
  *
- * @returns {Object} Object with shape `{operator: <string>, suffix: <number>}`
- * confirming learnt mathematical method.
+ * @returns {boolean} Indicates whether operator & method have been guessed.
  */
-export function guess ({ input, output }) {
-  // Represents guesses that have been made.
-  let guesses
+export function makeGuess ({ input, output }) {
+  let success = false
+
+  if (input < output) {
+    if (!guesses.length) {
+      guesses.push({ operator: OPERATORS.add, suffix: output - input })
+      guesses.push({ operator: OPERATORS.multiply, suffix: output / input })
+    } else {
+      guesses.filter(guess => {
+        /* eslint-disable-next-line no-eval */
+        return eval(`${input} ${guess.operator} ${guess.suffix} === ${output}`)
+      })
+
+      success = true
+    }
+  }
+
+  if (input > output) {
+    if (!guesses.length) {
+      guesses.push({ operator: OPERATORS.subtract, suffix: input - output })
+      guesses.push({ operator: OPERATORS.divide, suffix: input / output })
+    } else {
+      guesses.filter(guess => {
+        /* eslint-disable-next-line no-eval */
+        return eval(`${input} ${guess.operator} ${guess.suffix} === ${output}`)
+      })
+
+      success = true
+    }
+  }
+
+  if (input === output) {
+    guesses.push({ operator: OPERATORS.add, suffix: 0 })
+
+    success = true
+  }
+
+  return success
 }
 
 /**
@@ -48,7 +94,8 @@ export function guess ({ input, output }) {
  * confirming learnt mathematical method.
  */
 function learn ({ operator, suffix }) {
-
+  learntMethod.operator = operator
+  learntMethod.suffix = suffix
 }
 
 /**
@@ -57,19 +104,46 @@ function learn ({ operator, suffix }) {
  * @returns {number} Number representing input.
  */
 async function promptOperate () {
+  const input = await promptly.prompt('What is the input?')
 
+  return parseInt(input, 10)
 }
 
 /**
  * Operate on input using the learnt mathematical method.
  *
+ * @param {number} input - Input for the operation.
  * @param {Object} object - Parameter object.
- * @param {number} object.input - Input for the operation.
  * @param {string} object.operator - String representing operation.
  * @param {number} object.suffix - The suffix for the method.
  *
  * @returns {number} Output of operation.
  */
-export function operate ({input, operator, suffix}) {
+export function operate (input, {operator, suffix}) {
+  /* eslint-disable-next-line no-eval */
+  const result = eval(`${input} ${operator} ${suffix}`)
 
+  console.log(`${input} ${operator} ${suffix} = ${result}`)
+
+  return result
 }
+
+/**
+ * Initialize the learner.
+ *
+ * @returns {undefined} Undefined.
+ */
+async function init () {
+  let hasGuessed = false
+
+  while (!hasGuessed) {
+    const inOut = await promptGuess()
+    hasGuessed = makeGuess(inOut)
+  }
+
+  learn(guesses[guesses.length - 1])
+  const input = await promptOperate()
+  operate(input, learntMethod)
+}
+
+init()
